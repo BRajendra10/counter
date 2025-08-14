c73f1a9581eb00bb0eae903b99d110b5d695009d
Rohit-Pakhre <pakhrerohit281@gmail.com>
2025-08-14 17:32:43 +0530

    Counter ui done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-export const App = () => {
-  return <div>App</div>;
+import Counter from "./components/Counter";
+
+const App = () => {
+  return <main className="min-h-screen flex items-center justify-center bg-neutral-200">
+    <Counter />
+  </main>;
 };
+
+export default App;
